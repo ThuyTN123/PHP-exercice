13518f56b9eaf73bb4c3337680959f677d82a7a5
ThuyTN123 <nt.garciatran@it-students.fr>
2023-11-16 10:21:13 +0100

    test2

---
+++ test2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+$nun1=25;
+$nun1=50;
+
+
+
+
+$x = 0;
+ 
+while($x <= 100) {
+  echo "The number is: $x <br>";
+  $x+=10;
+}
+
+
+?>  
+
+</body>
+</html>
